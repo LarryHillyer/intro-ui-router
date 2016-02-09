30519503a702f220fe029deba331e77e3c696dd5
Jake Overall <joverall22@gmail.com>
2016-02-09 11:40:50 -0700

    adds basic auth and nested state

--- app/components/home/home-controller.js
+++ app/components/home/home-controller.js
@@ -1,3 +1,31 @@
+app.controller('MainController', function($scope, AuthService, $state){
+    
+    $scope.isLoggedIn = false;
+    
+    $scope.login = function(){
+        AuthService.login();
+        $scope.isLoggedIn = true;
+    }
+    
+    $scope.logout = function(){
+        AuthService.logout();
+        $scope.isLoggedIn = false;
+        $state.go('home');
+    }
+    
+    
+})
+
 app.controller('HomeController', function($scope){
     $scope.test = "Hello are you there";
+})
+
+app.service('AuthService', function(){
+    this.loggedIn = false;
+    this.login = function(){
+        this.loggedIn = true;
+    }
+    this.logout = function(){
+        this.loggedIn = false;
+    }
 })
--- app/config/routes.js
+++ app/config/routes.js
@@ -9,6 +9,19 @@ app.config(function($stateProvider){
       })
       .state('view2', {
          url: '/banana',
-         template: '<h1>HELLO FROM CHIM CHIM</h1> <a ui-sref="home">GO BACK</a>' 
+         template: '<h1>HELLO FROM CHIM CHIM</h1> <a ui-sref="home">GO BACK</a> <a ui-sref="auth.dashboard">GO to dashboard</a>' 
+      })
+      .state('auth', {
+          abstract: true,
+          template: '<ui-view></ui-view>',
+          controller: function($state, AuthService){
+              if(!AuthService.loggedIn){
+                  $state.go('home');
+              }
+          },
+      })
+      .state('auth.dashboard', {
+          url: '/dashboard',
+          template: '<h1>YOU ARE AWESOME and Authenticated</h1>'
       }) 
 })
